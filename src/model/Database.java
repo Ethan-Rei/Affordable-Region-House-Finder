712b58210d6c6844bdbd8686cb694c47bf68762c
Jhomar Gharbarran <jhomarg@my.yorku.ca>
2023-02-11 00:24:09 -0500

    Implementation of the mySQL database

--- src/model/Database.java
+++ src/model/Database.java
@@ -1,5 +1,5 @@
 package model;
 
 public interface Database {
-
+	public String query(String query);
 }
---
+++ src/utilities/Utilities.java
@@ -0,0 +1,15 @@
+package utilities;
+
+import model.Database;
+import model.MySQLDatabase;
+
+public class Utilities {
+
+	public static void main(String[] args) {
+		Database mySQL = new MySQLDatabase();
+		
+		String myQuery = "SELECT * FROM table";
+		System.out.println(mySQL.query(myQuery));
+	}
+
+}
