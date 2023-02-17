5711fcdf9a27dcc51a6277a4378162e5be4e6d07
Jhomar Gharbarran <jhomarg@my.yorku.ca>
2023-02-16 19:01:51 -0500

    importdb class

--- src/model/Database.java
+++ src/model/Database.java
@@ -4,6 +4,4 @@ import java.sql.ResultSet;
 
 public interface Database {
 	public ResultSet query(String query);
-	public boolean importData(String path, String table);
-	public boolean removeData(String table);
 }
