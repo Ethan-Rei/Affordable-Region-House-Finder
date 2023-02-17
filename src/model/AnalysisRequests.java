6ee22e8aeec61baa7776c073bcfd55d62971c5ba
Ethan-Rei <ethanrei@my.yorku.ca>
2023-02-16 21:04:54 -0500

    .

--- src/model/AnalysisRequests.java
+++ src/model/AnalysisRequests.java
@@ -1,17 +1,31 @@
 package model;
 
 import org.apache.commons.math3.stat.inference.TTest;
+import weka.core.Instances;
 import weka.classifiers.functions.LinearRegression;
 
 public class AnalysisRequests implements Requests 
 {
 	
 	private TTest tTest = new TTest();
+	private Instances testData;
+	private LinearRegression linearRegressionModel;
 	private static AnalysisRequests singleton = null;
 	
 	private AnalysisRequests() {
-		// Train the data on creation of object for now
+		// Train the model on creation of object for now
 		
+		// Create an array list of Attributes to train the model
+		// A string and the capacity of attributes
+		
+		testData = new Instances(null, null, 0);
+		linearRegressionModel = new LinearRegression();
+		try {
+			linearRegressionModel.buildClassifier(testData);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static AnalysisRequests getAnalysisRequests( ) {
