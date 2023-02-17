35287692422a6837e5a040b8024fb7b2b6fe1e01
Ethan-Rei <ethanrei@my.yorku.ca>
2023-02-16 19:43:29 -0500

    .

--- src/model/AnalysisRequests.java
+++ src/model/AnalysisRequests.java
@@ -1,19 +1,29 @@
 package model;
 
 import org.apache.commons.math3.stat.inference.TTest;
+import weka.classifiers.functions.LinearRegression;
 
 public class AnalysisRequests implements Requests 
 {
 	
+	private TTest tTest = new TTest();
+	private AnalysisRequests singleton;
 	
+	private AnalysisRequests() {
+		// Train the data on creation of object for now
+		
+	}
 	
-	
+	public AnalysisRequests getAnalysisRequests( ) {
+		if (singleton != null) { return new AnalysisRequests(); }
+		return singleton;
+	}
 	
 	public double tTest(double[] timeSeries1, double[] timeSeries2) {	
 		// Extract a double (type) array from each input time series
-		TTest tTest = new TTest();
+		
+		// Returns the p value of the TTest
 		return tTest.tTest(timeSeries1, timeSeries2);
 	}
-	
 	
 }
