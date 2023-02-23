fd51f3d638ad47d8643652c52f66f91d49acf266
Ethan-Rei <ethanrei@my.yorku.ca>
2023-02-23 00:23:22 -0500

    ,

--- src/analysis/Analysis.java
+++ src/analysis/Analysis.java
@@ -14,7 +14,7 @@ import weka.classifiers.functions.LinearRegression;
 public class Analysis
 {
 	// Better to put monthConverter in a date converter class
-	private TTest ttest;
+	private static TTest ttest;
 	private static Analysis singleton = null;
 	
 	private Analysis() {
@@ -27,7 +27,7 @@ public class Analysis
 	}
 	
 
-	public double tTest(double[] timeSeries1, double[] timeSeries2) {	
+	public static double tTest(double[] timeSeries1, double[] timeSeries2) {	
 		// Returns the p value of the TTest
 		return ttest.tTest(timeSeries1, timeSeries2);
 	}
@@ -38,9 +38,14 @@ public class Analysis
 		DatabaseQuery mysqlquery = new MySQLQuery(mysqlconnection);
 		ResultSet torontoSet = mysqlquery.query("Toronto, Ontario", "2000-01", "2020-01");
 		ResultSet hamiltonSet = mysqlquery.query("Hamilton, Ontario", "2000-01", "2020-01");
-		double ttestResult
+		double[] torontoArr = ResultSetToArray.getDoubleArray(torontoSet);
+		double[] hamiltonArr = ResultSetToArray.getDoubleArray(hamiltonSet);
+		double ttestResult = tTest(torontoArr, hamiltonArr);
 		
-		System.out.println();
-		if ()
+		System.out.println("Comparison between Toronto and Hamilton");
+		if (ttestResult < 0.5) {
+			System.out.println(String.format("Passed the ttest with p value of %f", ttestResult));
+		}
+		else { System.out.println(String.format("Failed the ttest with p value of %f", ttestResult)); }
 	}
 }
