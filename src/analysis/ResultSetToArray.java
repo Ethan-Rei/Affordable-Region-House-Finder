6b47223394b6fc251c8a91ddcb5682c08e030059
Jhomar Gharbarran <jhomarg@my.yorku.ca>
2023-02-23 00:15:53 -0500

    clean up getDoubleArray, fix statement

--- src/analysis/ResultSetToArray.java
+++ src/analysis/ResultSetToArray.java
@@ -4,20 +4,20 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class ResultSetToArray {
-	public static double[] getDoubleArray(ResultSet values) {
-		ArrayList<Double> doubleArrayList = new ArrayList<Double>();
-		double[] doubleArray;
+	public static double[] getDoubleArray(ResultSet values) {		
+		double[] doubleArray = null;
+		
 		try {
-			while (values.next()) {
-				doubleArrayList.add(values.getDouble("property_value"));
+			values.last();
+			doubleArray = new double[values.getRow()];
+			values.beforeFirst();
+			for(int i = 0; values.next(); i++) {
+				doubleArray[i] = values.getDouble("property_value");
 			}
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		doubleArray = new double[doubleArrayList.size()];
-		for (int i = 0; i < doubleArrayList.size(); i++) {
-			doubleArray[i] = doubleArrayList.get(i);
-		}
+		
 		return doubleArray;
 	}
 }
