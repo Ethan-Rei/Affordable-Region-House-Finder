d438640a85e11101f7403daadf489dd18deb1810
Ethan-Rei <ethanrei@my.yorku.ca>
2023-02-22 23:03:23 -0500

    .

--- src/analysis/Analysis.java
+++ src/analysis/Analysis.java
@@ -1,8 +1,5 @@
 package analysis;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -16,47 +13,11 @@ import weka.classifiers.functions.LinearRegression;
 public class Analysis
 {
 	// Better to put monthConverter in a date converter class
-	private HashMap<String, Integer> monthConverter;
-	private TTest tTest = new TTest();
-	private Instances testData;
-	private LinearRegression linearRegressionModel;
+	private TTest ttest;
 	private static Analysis singleton = null;
 	
 	private Analysis() {
-		// Setup the dictionary to reference
-		monthConverterSetup();
-
-		// Train the model on creation of object for now
 		
-		// Create an array list of Attributes to train the model
-		// A string and the capacity of attributes
-		
-		String filename = "MontrealHouseNHPI.txt";
-		ArrayList<Date> dates = new ArrayList<Date>();
-		ArrayList<Double> nhpis = new ArrayList<Double>();
-		
-        try {
-        	BufferedReader reader = new BufferedReader(new FileReader(filename));
-            String line;
-            String[] values;
-            while ((line = reader.readLine()) != null) {
-                values = line.split(" ", 0);
-                nhpis.add(Double.parseDouble(values[0]));
-                dates.add(convertDate(values[1]));
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-		
-		testData = new Instances(null, null, 0);
-		linearRegressionModel = new LinearRegression();
-		try {
-			linearRegressionModel.buildClassifier(testData);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 	
 	public static Analysis getAnalysisRequests() {
@@ -64,32 +25,6 @@ public class Analysis
 		return singleton;
 	}
 	
-	// We can remove this and put this in its own class
-	private void monthConverterSetup() {
-		monthConverter = new HashMap<String, Integer>();
-		monthConverter.put("Jan", 1);
-		monthConverter.put("Feb", 2);
-		monthConverter.put("Mar", 3);
-		monthConverter.put("Apr", 4);
-		monthConverter.put("May", 5);
-		monthConverter.put("Jun", 6);
-		monthConverter.put("Jul", 7);
-		monthConverter.put("Aug", 8);
-		monthConverter.put("Sep", 9);
-		monthConverter.put("Oct", 10);
-		monthConverter.put("Nov", 11);
-		monthConverter.put("Dec", 12);
-	}
-	
-	// Similarly with this
-	private Date convertDate(String strDate) {
-		String[] values = strDate.split(",", 0);
-		int year = Integer.parseInt(values[1]);
-		int month = monthConverter.get(values[0]);
-		Calendar calendar = Calendar.getInstance();
-		calendar.set((year < 23? year + 2000: year + 1900), month, 1);
-		return calendar.getTime();
-	}
 
 	public double tTest(double[] timeSeries1, double[] timeSeries2) {	
 		// Extract a double (type) array from each input time series
