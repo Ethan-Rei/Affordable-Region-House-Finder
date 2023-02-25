9e6d8167a91defcd0a8b2980bf560a1de0de7906
Ethan-Rei <ethanrei@my.yorku.ca>
2023-02-24 19:32:58 -0500

    Update Ui.java

--- src/ui/Ui.java
+++ src/ui/Ui.java
@@ -1,4 +1,5 @@
 package ui;
+
 import database.*;
 import java.sql.*;
 import javax.swing.*;
@@ -6,23 +7,20 @@ import org.jfree.chart.*;
 import java.awt.event.*;
 
 public class Ui {
-	
+
 	public static void main(String[] args) {
 		JFrame userView = new JFrame();
 		Database database = Database.getInstance();
-		ResultSet values = database.query("Toronto, Ontario", "2000-01", "2020-01");
-		JFreeChart lineChart = ResultSetTimeSeriesLineChart.getChart("Toronto, Ontario", values);
+		ResultSet values = database.query("Toronto, Ontario", "2019-01", "2020-01");
+		JFreeChart lineChart = ResultSetTimeSeriesLineChart.getChart("Toronto, Ontario from 2019-01 to 2020-01", values);
 		ChartPanel lineChartPanel = new ChartPanel(lineChart);
-		
-		userView.addWindowListener(
-				new WindowAdapter() {
-					public void windowClosing(WindowEvent e) { 
-						userView.dispose(); 
-						database.closeConnection();
-					}
-				}
-			);
-		
+		userView.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				userView.dispose();
+				database.closeConnection();
+			}
+		});
+
 		userView.add(lineChartPanel);
 		userView.setSize(400, 400);
 		userView.setVisible(true);
