7bd72e672c6736f342fe5c88d7ebd8bfdfa9abbc
Ethan-Rei <ethanrei@my.yorku.ca>
2023-02-24 19:48:08 -0500

    Update TimeSeriesLineChart.java

--- src/ui/TimeSeriesLineChart.java
+++ src/ui/TimeSeriesLineChart.java
@@ -1,6 +1,7 @@
 package ui;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
@@ -8,6 +9,9 @@ public class TimeSeriesLineChart{
 
 	public static JFreeChart getChart(String locationName, DefaultCategoryDataset data) {
 		JFreeChart linechart = ChartFactory.createLineChart(locationName, "Year", "NHPI", data, PlotOrientation.VERTICAL, true, true, false);
+		CategoryLabelPositions positions = CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 5.0);
+		linechart.getCategoryPlot().getDomainAxis().setCategoryLabelPositions(positions);
+		
 		return linechart;
 	}
 }
