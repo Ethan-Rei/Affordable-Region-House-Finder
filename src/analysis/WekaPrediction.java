d151d8a5a2b6e8a929265117f27a8c8445808524
Ethan-Rei <ethanrei@my.yorku.ca>
2023-03-29 04:50:36 -0400

    Finished up weka linear regression
     (still have to refactor and make better)

--- src/analysis/WekaPrediction.java
+++ src/analysis/WekaPrediction.java
@@ -1,11 +1,11 @@
 package analysis;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ArrayList;
 import weka.core.DenseInstance;
 import weka.core.Attribute;
 import weka.core.*;
+import java.util.Calendar;
 import weka.classifiers.functions.LinearRegression;
 import org.apache.commons.lang3.tuple.MutablePair;
 
@@ -15,8 +15,8 @@ class WekaPrediction implements TimeSeriesPrediction{
 	
 	public MutablePair<Double, Date>[] predict(double[] values, Date[] dates) {
 		
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM");
-		Attribute dateVals = new Attribute("Date", true);
+		Calendar calendar = Calendar.getInstance();
+		Attribute dateVals = new Attribute("Date");
 		Attribute nhpiVals = new Attribute("NHPI");
 		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
 		attributes.add(dateVals);
@@ -27,12 +27,11 @@ class WekaPrediction implements TimeSeriesPrediction{
 		System.out.println(dateVals.isString());
 		
 		for (int i = 0; i < values.length; i++) {
-			String dateString = dateFormat.format(dates[i]);
-			System.out.println(dateString);
 			Instance dataPoint = new DenseInstance(2);
+			calendar.setTime(dates[i]);
+			int numOfMonths = calendar.get(Calendar.YEAR) + calendar.get(Calendar.MONTH);
 			
-			
-			dataPoint.setValue(dateVals, dateString);
+			dataPoint.setValue(dateVals, numOfMonths);
 			dataPoint.setValue(nhpiVals, values[i]);
 			data.add(dataPoint);
 		}
@@ -49,6 +48,7 @@ class WekaPrediction implements TimeSeriesPrediction{
 		
 		
 		Instance predicationDataSet = data.get(0);
+		System.out.println(predicationDataSet);
 		try {
 			double value = regressionModel.classifyInstance(predicationDataSet);
 			System.out.println(value);
