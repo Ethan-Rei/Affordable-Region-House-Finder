4e255fe7a6821fb620b2788d68ad3fd881e0f24b
Jhomar Gharbarran <jhomarg@my.yorku.ca>
2023-04-04 05:54:57 -0400

    TimeSeries

--- src/windows/TimeSeries.java
+++ src/windows/TimeSeries.java
@@ -6,12 +6,25 @@ public class TimeSeries {
 	private String location;
 	private String startDate;
 	private String endDate;
-	private HashMap<String, Boolean> chartSettings = new HashMap<>();
+	private HashMap<ChartType, Boolean> chartSettings;
 	
 	public TimeSeries(String location, String startDate, String endDate) {
 		this.location = location;
 		this.startDate = startDate;
 		this.endDate = endDate;
+		insertSettings();
+	}
+	
+	private void insertSettings() {
+		chartSettings = new HashMap<>();
+		chartSettings.put(ChartType.LINE_CHART, false);
+		chartSettings.put(ChartType.PLOT_CHART, false);
+		chartSettings.put(ChartType.STACKED_AREA_CHART, false);
+		chartSettings.put(ChartType.HISTOGRAM_CHART, false);
+	}
+	
+	public boolean isChartEnabled(ChartType type) {
+		return chartSettings.get(type);
 	}
 
 	public String getLocation() {
@@ -22,11 +35,20 @@ public class TimeSeries {
 		return startDate;
 	}
 
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
 	public String getEndDate() {
 		return endDate;
 	}
 	
-	
-	
+	public String toString() {
+		return location + " (" + startDate + " to " + endDate + ")";
+	}
 	
 }
