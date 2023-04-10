5598f3dc87bacd882e2214af1cf8f6b1663c7b6a
Jhomar Gharbarran <jhomarg@my.yorku.ca>
2023-04-10 17:54:50 -0400

    Full refactor database package to remove code smell

--- src/database/MySQLQuery.java
+++ src/database/MySQLQuery.java
@@ -1,70 +1,26 @@
 package database;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class MySQLQuery {
 	private Statement statement;
 	
 	public MySQLQuery(DatabaseConnection connection) {
-		setConnection(connection.getConnection());
-	}
-
-	public void setConnection(Connection connection) {
 		try {
-			statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			statement = connection.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public ArrayList<String> queryLocations() {
-		String query = "SELECT DISTINCT location_name FROM data";
-		ArrayList<String> locations = new ArrayList<>();
-		try {
-			ResultSet returned = statement.executeQuery(query);
-			while(returned.next()) {
-				locations.add(returned.getString(1));
-			}
-			Collections.sort(locations);
-			return locations;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return null;
 	}
 	
-	public ArrayList<String> queryTimes() {
-		String query = "SELECT DISTINCT refdate FROM data";
-		ArrayList<String> times = new ArrayList<>();
-		try {
-			ResultSet returned = statement.executeQuery(query);
-			while(returned.next()) {
-				times.add(returned.getString(1).substring(0, returned.getString(1).length()-3));
-			}
-			Collections.sort(times);
-			return times;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return null;
-	}
-	
-	public ResultSet queryNHPI(String locationName, String fromDate, String toDate) {
-		String locName = locationName.replace("'", "''");
-		String query = String.format("SELECT refdate, property_value FROM data WHERE location_name='%s' AND refdate BETWEEN '%s-01' AND '%s-01'", locName, fromDate, toDate);
+	public ResultSet query(String query) {
 		try {
 			return statement.executeQuery(query);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 		return null;
 	}
 
---
+++ src/database/MySQLQueryDecorator.java
@@ -0,0 +1,82 @@
+package database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+
+import windows.WindowHelper;
+
+public class MySQLQueryDecorator implements DatabaseQuery {
+	
+	private MySQLQuery query;
+	
+	public MySQLQueryDecorator (DatabaseConnection connection) {
+		this.query = new MySQLQuery(connection);
+	}
+
+	@Override
+	public ArrayList<String> getAllLocations() {
+		String queryLoc = "SELECT DISTINCT location_name FROM data";
+		int locColumn = 1;
+		
+		ResultSet returned = query.query(queryLoc);
+		ArrayList<String> locations = new ArrayList<>();
+		
+		try {
+			while(returned.next()) {
+				locations.add(returned.getString(locColumn));
+			}
+			Collections.sort(locations);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return locations;
+	}
+
+	@Override
+	public ArrayList<String> getAllTimes() {
+		String queryTimes = "SELECT DISTINCT refdate FROM data";
+		int timeColumn = 1;
+		
+		ArrayList<String> times = new ArrayList<>();
+		ResultSet returned = query.query(queryTimes);
+		try {
+			while(returned.next()) {
+				String row = returned.getString(timeColumn);
+				times.add(row.substring(0, row.length() - 3));
+			}
+			Collections.sort(times);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return times;
+	}
+
+	@Override
+	public HashMap<Date, Double> getNHPI(String locationName, String fromDate, String toDate) {
+		String locName = locationName.replace("'", "''");
+		String queryNHPI = String.format("SELECT refdate, property_value FROM data WHERE location_name='%s' AND refdate BETWEEN '%s-01' AND '%s-01'", locName, fromDate, toDate);
+		int dateColumn = 1;
+		int nhpiColumn = 2;
+		
+		ResultSet returned = query.query(queryNHPI);
+		HashMap<Date, Double> nhpiValues = new HashMap<>();
+		try {
+			while (returned.next()) {
+				String rowDate = returned.getString(dateColumn);
+				Date refdate = WindowHelper.dateFormat.parse(rowDate.substring(0, rowDate.length() - 3));
+				nhpiValues.put(refdate, returned.getDouble(nhpiColumn));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return nhpiValues;
+	}
+
+}
